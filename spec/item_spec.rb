862f153f783b0e940a4e22954f6c1ab29f3bcb87
Evan Thomas <evanthomas0292@gmail.com>
2023-06-23 13:41:21 +0100

    Put Item class into its own file and write unit test.

---
+++ spec/item_spec.rb
@@ -0,0 +1,8 @@
+require 'item'
+
+RSpec.describe Item do
+  it 'has a string representation with name, sell_in, and quality' do
+    item = Item.new('test_item', 10, 20)
+    expect(item.to_s).to eq 'test_item, 10, 20'
+  end
+end
