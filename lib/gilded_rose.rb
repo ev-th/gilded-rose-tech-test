d047a8144560839a7e2476cce8011941a825a630
Evan Thomas <evanthomas0292@gmail.com>
2023-06-23 19:44:29 +0100

    Copy quality updating logic into another file for special items. Main file only uses standard updater for standard items.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,14 +1,25 @@
 require 'quality_updater'
+require 'special_item_quality_updater'
 
 class GildedRose
   def initialize(items)
     @items = items
     @updater = QualityUpdater.new
+    @special_updater = SpecialItemQualityUpdater.new
+    @special_items = [
+      'Sulfuras, Hand of Ragnaros',
+      'Backstage passes to a TAFKAL80ETC concert',
+      'Aged Brie'
+    ]
   end
 
   def update_quality()
     @items.each do |item|
-      @updater.update_item(item)
+      if @special_items.include?(item.name)
+        @special_updater.update_item(item)
+      else
+        @updater.update_item(item)
+      end
     end
   end
 end
