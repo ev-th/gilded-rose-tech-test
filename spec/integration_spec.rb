0fbd7705dd2a2b1cf61d6368e31dc7dd209e0d17
Evan Thomas <evanthomas0292@gmail.com>
2023-06-24 18:00:19 +0100

    Add conjured item to 'update all products' integration test. Refactor tests for readability

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,63 +2,61 @@ require 'gilded_rose'
 require 'item'
 
 describe 'integration' do
-  let(:standard_item) { Item.new('standard_item', 10, 20) }
-  let(:old_item) { Item.new('old_item', 0, 15) }
-  let(:garbage_item) { Item.new('garbage_item', -1, 1) }
-  let(:quality_item) { Item.new('quality_item', 10, 51) }
-  let(:aged_brie) { Item.new('Aged Brie', 20, 49) }
-  let(:sulfuras) { Item.new('Sulfuras, Hand of Ragnaros', 30, 40) }
-  let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 13) }
-  let(:valuable_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 48) }
-  let(:today_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 40) }
-  let(:pass_49_quality) { Item.new('Backstage passes to a TAFKAL80ETC concert', 7, 49) }
-  let(:pass_48_quality) { Item.new('Backstage passes to a TAFKAL80ETC concert', 3, 48) }
-
   describe 'GildedRose#update_quality' do
     context 'for standard items' do
       it 'reduces sell_by and quality by 1' do
-        gilded_rose = GildedRose.new([standard_item])
+        item = Item.new('standard_item', 10, 20)
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
-        expect(standard_item.to_s).to eq 'standard_item, 9, 19'
+        expect(item.to_s).to eq 'standard_item, 9, 19'
       end
       
       it 'reduces quality twice as fast once sell by date is passed' do
-        gilded_rose = GildedRose.new([old_item])
+        item = Item.new('old_item', 0, 15)
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
-        expect(old_item.to_s).to eq 'old_item, -1, 13'
+        expect(item.to_s).to eq 'old_item, -1, 13'
       end
       
       it 'never reduces quality past 0' do
-        gilded_rose = GildedRose.new([garbage_item])
+        item = Item.new('garbage_item', -1, 1)
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
-        expect(garbage_item.to_s).to eq 'garbage_item, -2, 0'
+        expect(item.to_s).to eq 'garbage_item, -2, 0'
       end
     end
     
     context "For 'Aged Brie'" do
+      let(:item) { Item.new('Aged Brie', 20, 49) }
+
       it 'increases quality by 1 and decreases sell_by by 1' do
-        gilded_rose = GildedRose.new([aged_brie])
+        item = Item.new('Aged Brie', 20, 49)
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
-        expect(aged_brie.to_s).to eq 'Aged Brie, 19, 50'
+        expect(item.to_s).to eq 'Aged Brie, 19, 50'
       end
 
       it 'cannot increase quality above 50' do
-        gilded_rose = GildedRose.new([aged_brie])
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
         gilded_rose.update_quality
-        expect(aged_brie.to_s).to eq 'Aged Brie, 18, 50'
+        expect(item.to_s).to eq 'Aged Brie, 18, 50'
       end
     end
     
     context "For 'Sulfuras'" do
       it 'never decreases quality or sell by' do
-        gilded_rose = GildedRose.new([sulfuras])
+        item = Item.new('Sulfuras, Hand of Ragnaros', 30, 40)
+        gilded_rose = GildedRose.new([item])
         gilded_rose.update_quality
-        expect(sulfuras.to_s).to eq 'Sulfuras, Hand of Ragnaros, 30, 40'
+        expect(item.to_s).to eq 'Sulfuras, Hand of Ragnaros, 30, 40'
       end
     end
     
     context 'For Backstage passes' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 13) }
+      let(:valuable_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 48) }
+
       it 'increases in quality by 1 when sell_in is more than 10 days' do
         gilded_rose = GildedRose.new([backstage_pass])
         gilded_rose.update_quality
@@ -79,9 +77,10 @@ describe 'integration' do
       end
 
       it 'reduces quality to 0 when sell_by reduces to 0' do
-        gilded_rose = GildedRose.new([today_pass])
+        pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 40)
+        gilded_rose = GildedRose.new([pass])
         gilded_rose.update_quality
-        expect(today_pass.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, -1, 0'
+        expect(pass.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, -1, 0'
       end
 
       it 'cannot increase quality above 50' do
@@ -92,15 +91,17 @@ describe 'integration' do
       end
 
       it 'if quality tries to increase by 2 from 49, it will stop at 50' do
-        gilded_rose = GildedRose.new([pass_49_quality])
+        pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 7, 49)
+        gilded_rose = GildedRose.new([pass])
         gilded_rose.update_quality
-        expect(pass_49_quality.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 6, 50'
+        expect(pass.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 6, 50'
       end
 
       it 'if quality tries to increase by 3 from 48, it will stop at 50' do
-        gilded_rose = GildedRose.new([pass_48_quality])
+        pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 3, 48)
+        gilded_rose = GildedRose.new([pass])
         gilded_rose.update_quality
-        expect(pass_48_quality.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 2, 50'
+        expect(pass.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 2, 50'
       end
     end
 
@@ -132,8 +133,14 @@ describe 'integration' do
     end
 
     it 'updates all the products in the items list' do
+      standard_item = Item.new('standard_item', 10, 20)
+      aged_brie = Item.new('Aged Brie', 20, 49)
+      sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 30, 40)
+      backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 13)
+      conjured_item = Item.new('Conjured Item', 10, 13)
+
       gilded_rose = GildedRose.new(
-        [standard_item, aged_brie, sulfuras, backstage_pass]
+        [standard_item, aged_brie, sulfuras, backstage_pass, conjured_item]
       )
       gilded_rose.update_quality
 
@@ -141,6 +148,7 @@ describe 'integration' do
       expect(aged_brie.to_s).to eq 'Aged Brie, 19, 50'
       expect(sulfuras.to_s).to eq 'Sulfuras, Hand of Ragnaros, 30, 40'
       expect(backstage_pass.to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 10, 14'
+      expect(conjured_item.to_s).to eq 'Conjured Item, 9, 11'
     end
   end
 end
